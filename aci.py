1d66faae6cb5ca10786b4e80aa8a8864b252661e
Pieter <plewyllie@gmail.com>
2018-07-15 10:38:19 -0700

    tt

--- aci.py
+++ aci.py
@@ -15,3 +15,18 @@ response = requests.request("POST", url, data=payload, verify=False, headers=hea
 
 
 print(response.text)
+
+url = "https://apic/api/node/mo/uni/tn-Shipping.json"
+
+payload = "{\"fvTenant\":{\"attributes\":{\"dn\":\"uni/tn-Shipping\",\"name\":\"Shipping\",\"rn\":\"tn-Shipping\",\"status\":\"created\"},\"children\":[]}}"
+headers = {
+    'content-type': "application/json",
+    'cache-control': "no-cache",
+    'postman-token': "78f158b1-451f-cbac-86eb-bd8e104ddd6a"
+    }
+
+response = requests.request("POST", url, data=payload, headers=headers)
+
+print(response.text)
+
+
