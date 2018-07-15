fc0dc301bc89ebff73b60fd6e0da2e782c979aab
Pieter <plewyllie@gmail.com>
2018-07-15 10:27:03 -0700

    aci

---
+++ aci.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://apic/api/aaaLogin.json"
+
+payload = "{\"aaaUser\":{\"attributes\":{\"name\":\"admin\",\"pwd\":\"cisco123\"}}}"
+headers = {
+    'content-type': "application/json",
+    'cache-control': "no-cache",
+    'postman-token': "91b8b646-6a08-d6f0-41fb-8cdf4c245579"
+    }
+
+response = requests.request("POST", url, data=payload, headers=headers)
+
+print(response.text)
