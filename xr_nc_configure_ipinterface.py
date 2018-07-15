7fbbd2fcc3fa026bd7081dd0bc99d67d7e810e24
Pieter Lewyllie <plewyllie@gmail.com>
2018-07-15 15:32:51 +0200

    secondaries

--- xr_nc_configure_ipinterface.py
+++ xr_nc_configure_ipinterface.py
@@ -23,11 +23,13 @@ if __name__ == "__main__":
                    <address>10.23.23.1</address>
                    <netmask>255.255.255.0</netmask>
                   </primary>
+                  <secondaries>
+                   <secondary>
+                    <address>20.32.32.1</address>
+                    <netmask>255.255.255.0</address>
+                   </secondary>
+                  </secondaries> 
                  </addresses>
-                 <secondary>
-                  <address>20.32.32.1</address>
-                  <netmask>255.255.255.0</address>
-                 </secondary>
                 </ipv4-network>
                </interface-configuration>
               </interface-configurations>
